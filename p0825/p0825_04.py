16eefca0be975ab3808871fcdcbb627337126fd1
minsol <minsol2946@gmail>
2026-08-26 10:22:13 +0900

    print,변수,타입-pythonclass1

---
+++ p0825/p0825_04.py
@@ -0,0 +1,46 @@
+
+# #변수: 값을 저장하는 공간
+# # 타입: 값을 입력할 때 정해짐 - 성수타입, 실수타입, 문자열타입, 불타입
+# a = 10 # 숫자형 타입 - 정수타입
+# b = 10.1 # 숫자형 타입 - 실수타입, 소수점
+# aa = "안녕" # 문자열 타입
+# abc = True # 불타입(bool) - True, False / boolean
+# #예약어는 변수로 사용할 수 없다.
+
+# print(10+5)
+# print(10-5)
+# print(10*5)
+# print(10/5)
+# print(10//5) #몫 2
+# print(10%5) #나머지 0
+# print(10**5) #제곱 100000
+
+# print(9+4)
+# print(9-4)
+# print(9*4)
+# print(9/4)
+# print(9//4)
+# print(9%4)
+# print(9**4)
+
+# a = 10
+# b = 5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# 타입 확인
+a = 100
+b = 10.1
+c = "안녕"
+d =True
+# c가 무슨 타입??
+#타입 확인 방법 type(a)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
